ffc20d178ae5d12f6203f5666031de5391cc7c03
DennisDavidAnchundia <dennisad3099@gmail.com>
2026-01-12 14:28:46 -0500

    Listo para produccion

---
+++ controllers/producto.controller.js
@@ -0,0 +1,116 @@
+// controllers/productos.js
+const Producto = require('../models/Producto');
+const cloudinary = require('cloudinary').v2;
+cloudinary.config();
+
+const crearProducto = async (req, res) => {
+    const { nombre, descripcion, precio } = req.body;
+
+    try {
+        let urlImagen = '';
+
+        if (req.files && req.files.archivo) {
+            const { tempFilePath } = req.files.archivo;
+            const { secure_url } = await cloudinary.uploader.upload(tempFilePath, {
+                folder: 'restaurante/productos'
+            });
+            urlImagen = secure_url;
+        }
+
+        const nuevoProducto = new Producto({
+            nombre,
+            descripcion,
+            precio,
+            imagen: urlImagen
+        });
+
+        await nuevoProducto.save();
+
+        res.status(201).json({
+            ok: true,
+            producto: nuevoProducto
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Error al crear el producto' });
+    }
+};
+const traerProductos = async (req, res) => {
+
+    try {
+       const productos = await Producto.find()
+        res.status(201).json({
+            ok: true,
+            productos
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const obtenerMasVendidos = async (req, res) => {
+    try {
+        const productos = await Producto.find()
+            .sort({ ventasTotales: -1 })
+            .limit(6);
+
+        res.json({ ok: true, productos });
+    } catch (error) {
+        res.status(500).json({ ok: false, msg: 'Error al obtener más vendidos' });
+    }
+};
+
+const obtenerMejorRating = async (req, res) => {
+    try {
+        const productos = await Producto.find()
+            .sort({ ratingPromedio: -1 })
+            .limit(6);
+
+        res.json({ ok: true, productos });
+    } catch (error) {
+        res.status(500).json({ ok: false, msg: 'Error al obtener mejores calificados' });
+    }
+};
+
+const calificarProducto = async (req, res) => {
+    const { id } = req.params; 
+    const { rating } = req.body;
+    const usuarioId = req.usuario._id; 
+    try {
+        const producto = await Producto.findById(id);
+
+        const yaVoto = producto.usuariosQueCalificaron.some(uid => uid.toString() === usuarioId.toString());
+  if (yaVoto) {
+    return res.status(400).json({ 
+        ok: false, 
+        msg: 'Ya has calificado este producto' 
+    });
+}
+
+        const nuevoNumRevisiones = producto.numRevisiones + 1;
+        const nuevoRatingPromedio = ((producto.ratingPromedio * producto.numRevisiones) + rating) / nuevoNumRevisiones;
+
+        const productoActualizado = await Producto.findByIdAndUpdate(
+            id, 
+            { 
+                ratingPromedio: nuevoRatingPromedio.toFixed(1),
+                numRevisiones: nuevoNumRevisiones,
+                $push: { usuariosQueCalificaron: usuarioId } 
+            }, 
+            { new: true }
+        );
+
+        res.json({ ok: true, producto: productoActualizado });
+    } catch (error) {
+        res.status(500).json({ ok: false, msg: 'Error al calificar' });
+    }
+};
+module.exports = {
+    crearProducto,
+    traerProductos,
+    obtenerMasVendidos,
+    obtenerMejorRating, 
+    calificarProducto
+}
---
+++ routes/producto.router.js
@@ -0,0 +1,30 @@
+// routes/productos.js
+const { Router } = require('express');
+const { validarJWT } = require('../middlewares/validar-JWT');
+const { isWorkerRole } = require('../middlewares/isAdminRegister');
+const { crearProducto, traerProductos, obtenerMasVendidos, obtenerMejorRating, calificarProducto } = require('../controllers/producto.controller');
+const { validarCampos } = require('../middlewares/erros.middlewares');
+const { check } = require('express-validator');
+
+const router = Router();
+
+router.post('/crearProducto', [
+    validarJWT,
+    isWorkerRole, 
+], crearProducto);
+router.get('/traerProducto', [
+    validarJWT,
+], traerProductos);
+router.get('/TraerProductosHomePublic', [
+    
+], traerProductos);
+router.get('/masvendidos', obtenerMasVendidos);
+
+router.get('/mejor-calificados', obtenerMejorRating);
+router.put('/calificar/:id', [
+validarJWT,
+    check('rating', 'El rating es obligatorio y debe ser entre 1 y 5').isFloat({ min: 1, max: 5 }),
+    validarCampos
+],
+   calificarProducto );
+module.exports = router;
